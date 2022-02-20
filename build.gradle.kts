43dffa0b149958d86df93778ebf175f1fc4b43d9
hojin-choi <hojin.joseph.choi@gmail.com>
2022-02-20 22:03:40 +0900

    chore: ktlint, junit 등 gradle 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    `java-library`
     kotlin("jvm") version "1.5.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.2.1"
 }
-
-
 
 allprojects {
     group = "dev.hojin.kotlinsample"
@@ -10,6 +13,20 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        filter {
+            exclude("*.kts")
+        }
+    }
+}
+
+tasks {
+    register<Exec>("lint") {
+        commandLine = "./gradlew ktlintCheck".split(" ")
     }
 }
 
@@ -20,5 +37,23 @@ subprojects {
 
     dependencies {
         implementation(kotlin("stdlib"))
+        testImplementation(platform("org.junit:junit-bom:5.8.2"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.hamcrest:hamcrest:2.2")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+
+    // JSR-305 에 정의된 null 허용성 어노테이션 강제
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
     }
 }
